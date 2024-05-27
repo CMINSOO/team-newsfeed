447b62982d80f4b99e044502155815a780ae7dcc
minsooC <soochomin@gmail.com>
2024-05-27 19:25:08 +0900

    fix: 로그인 api 버그로 인한 재생성

--- src/routers/users.routers.js
+++ src/routers/users.routers.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { prisma } from "../utils/prisma.util.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
+const ACCESS_TOKEN_SECRET_KEY = "testing123";
 
 // 회원가입API
 
@@ -60,6 +64,43 @@ router.post("/sign-up", async (req, res, next) => {
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
     },
+  });
+});
+
+// 로그인 api / ACCESSTOKEN 발급
+function createAccessToken(id) {
+  return jwt.sign({ id: id }, ACCESS_TOKEN_SECRET_KEY, {
+    expiresIn: "12h",
+  });
+}
+
+router.post("/sign-in", async (req, res, next) => {
+  const { email, password } = req.body;
+  // 이메일을 입력안했을경우
+  if (!email) {
+    return res.status(400).json({ error: "email 을 입력해주세요" });
+  }
+  // 패스워드를 입력 안했을경우
+  if (!password) {
+    return res.status(400).json({ error: "비밀번호를 입력해주세요." });
+  }
+  // 이메일 형식이 올바르지 않을경우
+  // 이메일로 조회되지 않거나 비밀번호가 일치하지 않는 경우
+  const crosscheck = await prisma.users.findFirst({
+    where: { email },
+  });
+  if (!crosscheck) {
+    return res.status(401).json({ message: "존재하지 않는 이메일입니다" });
+  }
+  const result = await bcrypt.compare(password, crosscheck.password);
+  if (!result) {
+    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다" });
+  }
+  // AccessToken(Payload에 사용자 ID를 포함하고, 유효기한이 12시간)을 생성합니다.
+  const token = createAccessToken(crosscheck.userId);
+  res.cookie("authorization", `Bearer ${token}`);
+  return res.status(200).json({
+    message: "로그인에 성공하였습니다.",
   });
 });
 
