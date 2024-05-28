a377931c45cfbd8e3dc9d8b52eb802b80fb90dd2
minsooC <soochomin@gmail.com>
2024-05-28 19:16:33 +0900

    이력서 상세조회 api 구현

--- src/routers/resumes.router.js
+++ src/routers/resumes.router.js
@@ -61,4 +61,40 @@ router.get("/listing", authMiddleware, async (req, res) => {
     .json({ message: "이력서 목록 조회에 성공하였습니다", data: resumes });
 });
 
+// 이력서 상세 조회 APi
+router.get("/reading/:resumeId", authMiddleware, async (req, res) => {
+  // 사용자가져오기
+  const user = req.user;
+  //   이력서 id 가져오기
+  const { resumeId } = req.params;
+
+  const crosscheck = await prisma.resume.findFirst({
+    where:
+      user.role === "APPLICANT"
+        ? { resumeId: resumeId }
+        : { resumeId: +resumeId, UserId: +user.userId },
+    select: {
+      resumeId: true,
+      User: {
+        select: {
+          UserInfos: {
+            select: { name: true },
+          },
+        },
+      },
+      title: true,
+      content: true,
+      state: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  if (!crosscheck) {
+    return res.status(401).json({ message: "이력서가 존재하지 않습니다" });
+  }
+  return res
+    .status(200)
+    .json({ message: "이력서 상세조회에 성공했습니다", data: { crosscheck } });
+});
+
 export default router;
