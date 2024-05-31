a3f6c9d2481aecbbb743e4c77ef79cb92774f53f
minsooC <soochomin@gmail.com>
2024-05-31 15:25:22 +0900

    feat: validator/constants 생성

--- src/constants/env.constants.js
+++ src/constants/env.constants.js
@@ -1,3 +1,5 @@
 import "dotenv/config";
 
 export const SERVER_PORT = process.env.SERVER_PORT;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const TOKEN_EXPIREDIN = "12h";
---
+++ src/constants/message.constant.js
@@ -0,0 +1,35 @@
+export const MESSAGES = {
+  AUTH: {
+    COMMON: {
+      EMAIL: {
+        REQUIRED: "이메일을 입력해주세요",
+        INVALID_FORMAT: "이메일 형식에 맞게 입력햐주새요",
+        DUPLICATE_EMAIL: "이미 가입된 사용자입니다.",
+      },
+      PASSWORD: {
+        REQUIRED: "비밀번호를 입력해 주세요.",
+        LENGTH: `비밀번호는 6자리 이상이어야 합니다.`,
+      },
+      PASSWORD_CONFIRM: {
+        REQUIRED: "확인비밀번호를 입력해 주세요.",
+        NOT_MATCHED_WITH_PASSWORD: "입력한 두 비밀번호가 일치하지 않습니다",
+      },
+      NAME: {
+        REQUIRED: "이름을 입력해주세요.",
+      },
+      UNAUTHENTICATED: "인증정보가 유효하지 않습니다.",
+      JWT: {
+        NO_TOKEN: "인증 정보가 없습니다.",
+        NOT_SUPPORTED_TYPE: "지원하지 않는 인증 방식입니다.",
+        EXPIRED: "인증 정보가 만료 되었습니다",
+        NO_USER: "인증 정보와 일치하는 사용자가 없습니다.",
+      },
+    },
+    SIGN_UP: {
+      SUCCEED: " 회원가입에 성공하였습니다.",
+    },
+    SIGN_IN: {
+      SUCCEED: "로그인에 성공하였습니다.",
+    },
+  },
+};
