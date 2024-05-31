eacb468180236414af3f9c5ad41b200b60e5177e
minsooC <soochomin@gmail.com>
2024-05-31 16:03:11 +0900

    fix prisma 수정

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,2 +1,15 @@
+import express from "express";
+import { prisma } from "../utils/prisma.util.js";
+import { signUpValidator } from "../middlewares/validators/sign-up-validator.middleware.js";
 
+const authRouter = express.Router();
 
+// 회원가입 api
+authRouter.post("/sign-up", signUpValidator, async (req, res, next) => {
+  try {
+  } catch (error) {
+    next();
+  }
+});
+
+export { authRouter };
