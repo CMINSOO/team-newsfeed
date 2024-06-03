e980a18a6f6c7f8fc133516c4d266cc28f1909fd
jeonsuwon <swjeon0330@gmail.com>
2024-06-03 11:57:12 +0900

    before pull

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/error-handling.middleware.js";
 import { SERVER_PORT } from "./constants/env.constants.js";
+import { authRouter } from "./routers/auth.router.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api", [authRouter]);
 app.use(errorHandler);
 
-app.listen(SERVER_PORT, () => {
-  console.log(`서버가 ${SERVER_PORT}번 포트에서 실행중입니다`);
+app.listen(3000, () => {
+  console.log(`서버가 3000번 포트에서 실행중입니다`);
 });
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,12 +1,46 @@
 import express from "express";
 import { prisma } from "../utils/prisma.util.js";
 import { signUpValidator } from "../middlewares/validators/sign-up-validator.middleware.js";
+import bcrypt from "bcrypt";
 
 const authRouter = express.Router();
 
 // 회원가입 api
-authRouter.post("/sign-up", signUpValidator, async (req, res, next) => {
+authRouter.post("/sign-up", async (req, res, next) => {
   try {
+    const { name, email, password, nickname, role } = req.body;
+
+    const existEmail = await prisma.users.findFirst({
+      where: { email },
+    });
+
+    if (existEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: "이미 존재하는 이메일입니다." });
+    }
+
+    const user = await prisma.users.create({
+      data: {
+        email,
+        password,
+      },
+    });
+
+    const userinfo = await prisma.userInfos.create({
+      data: {
+        UserId: user.userId,
+        email,
+        name,
+        nickname,
+        role,
+        createdAt,
+        updatedAt,
+      },
+    });
+    return res
+      .status(200)
+      .json({ message: "회원가입이 완료가 되었습니다.", data: userinfo });
   } catch (error) {
     next();
   }
