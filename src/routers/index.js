abaf8ab873037be1f6f4868a2e389e6bd2b9ef48
minsooC <soochomin@gmail.com>
2024-06-04 10:05:02 +0900

    feat: userROuter 생성

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import { authRouter } from "./auth.router.js";
 import { postRouter } from "./post.router.js";
 import { commentRouter } from "./comments.router.js";
+import { userRouter } from "./users.router.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/post", postRouter);
 apiRouter.use("/post", commentRouter);
+apiRouter.use("/user", userRouter);
 
 export { apiRouter };
---
+++ src/routers/users.router.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { prisma } from "../utils/prisma.util.js";
+
+const userRouter = express.Router();
+
+// 사용자 조회
+userRouter.get("/user/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  const user = await prisma.user.findFirst({
+    where: { id: +id },
+    //특정 컬럼만 조회하는 파라미터
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      name: true,
+      nickname: true,
+      role: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+  //3.조회한 사용자의 상세한 정보를 클라이언트에게 반환합니다.
+  return res.status(200).json({ data: user });
+});
+
+export { userRouter };
