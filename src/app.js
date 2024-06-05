5d2ab4a2cf9b2b4fabfeb69ff62eab11138f5e2c
kimyounggyu <ykyu99@naver.com>
2024-06-05 11:06:09 +0900

    feat: 24.06.05 email 인증 완료 김영규

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/error-handling.middleware.js";
 import { SERVER_PORT } from "./constants/env.constants.js";
 import { apiRouter } from "./routers/index.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -11,6 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api", [apiRouter]);
 app.use(errorHandler);
+
+
+// 정적 파일 서빙 설정
+app.use(express.static(path.join(__dirname, 'asset')));
+
+// 라우팅
+app.get('/api/mail', (req, res) => {
+  res.sendFile(path.join(__dirname, 'asset', 'mailAuth.html'));
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`서버가 ${SERVER_PORT}번 포트에서 실행중입니다`);
--- src/constants/env.constants.js
+++ src/constants/env.constants.js
@@ -6,3 +6,6 @@ export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 export const TOKEN_EXPIREDIN = "12h";
 export const REFRESH_TOKEN_EXPIREDIN = "7d";
+export const EMAIL_SERVICE = process.env.EMAIL_SERVICE;
+export const EMAIL_USER = process.env.EMAIL_USER;
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
